f892c105786b3feba0e5c853f35b935748cdceff
awallef <antoine.wallef@gmail.com>
2021-02-18 08:17:59 +0100

    more

--- resources/assets/store/ormPlugin.js
+++ resources/assets/store/ormPlugin.js
@@ -1,14 +1,10 @@
 // VUEX ORM
 import VuexORM from '@vuex-orm/core'
 import VuexORMCRUD from '@wgr-sa/vuex-orm-crud'
-//import VuexORMRelations from '@wgr-sa/vuex-orm-relations'
-//import VuexORMCRUDUI from '@wgr-sa/vuex-orm-crud-ui'
 import database from '../database'
 import client from '../http/client'
 
 // init ORM-CRUD
 VuexORM.use(VuexORMCRUD, {client})
-//VuexORM.use(VuexORMCRUDUI)
-//VuexORM.use(VuexORMRelations)
 
 export default VuexORM.install(database)
