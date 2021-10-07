c9c72c2c78f8e096142b6195912dae47d9ddfdff
awallef <antoine.wallef@gmail.com>
2021-10-07 15:53:12 +0200

    slug

--- src/Model/Behavior/PageSlugBehavior.php
+++ src/Model/Behavior/PageSlugBehavior.php
@@ -5,16 +5,17 @@ use ArrayObject;
 
 use Cake\Event\Event;
 use Cake\Datasource\EntityInterface;
+use Cake\Utility\Text;
 
 use Trois\Utils\ORM\Behavior\SluggableBehavior;
 
 class PageSlugBehavior extends SluggableBehavior
 {
-  protected $refs = [];
+  protected $entity;
 
-  protected function getParentSlug($id, $locale = null)
+  protected function getParentSlug($locale = null)
   {
-    $entity = $this->$refs[$id];
+    $entity = $this->entity;
     if(!$entity->parent_id) return '';
 
     $opts = $locale? ['finder' => 'translations']: [];
@@ -26,17 +27,17 @@ class PageSlugBehavior extends SluggableBehavior
   public function slug(Event $event, EntityInterface $entity, $fields, $slug)
   {
     // store for later use : )
-    $this->$refs[$entity->{$this->getTable()->getPrimarykey()}] = $entity;
+    $this->entity = $entity;
     parent::slug($event, $entity, $fields, $slug);
   }
 
   public function _generate_slug($id, $value, $locale = null )
   {
     $slug = strtolower(Text::slug($value, $this->getConfig('replacement')));
-    $parentSlug = $this->getParentSlug($id, $locale);
+    $parentSlug = $this->getParentSlug($locale);
     $slug = "$parentSlug/$slug";
 
     if (strlen($slug) > $this->getConfig('max_length')) $slug = substr($slug, 0, $this->getConfig('max_length'));
-    return $this->_deduplicate_slug($id, $slug, $field, $locale);
+    return $this->_deduplicate_slug($id, $slug, $this->getConfig('field'), $locale);
   }
 }
